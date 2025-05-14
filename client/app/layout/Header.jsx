7be0e940bb94488b160d7f72bbb88e0a91b50613
Renato <renato.vergara.dev@gmail.com>
2025-05-14 22:33:36 +0200

    feat(header): update Header component with icons for Github and Notion links

--- client/app/layout/Header.jsx
+++ client/app/layout/Header.jsx
@@ -1,11 +1,22 @@
+import { FaGithub } from "react-icons/fa";
+import { RiNotionFill } from "react-icons/ri";
+
 export default function Header() {
     return (
-        <header className="header-layout bg-zinc-900 grid grid-cols-2 px-20 py-5">
+        <header className="header-layout bg-zinc-900 grid grid-cols-2 px-20 py-5 items-center">
             <h1>Logo placeholder</h1>
             <div className="justify-self-end-safe">
                 <ul className="flex gap-4">
-                    <li><a href="https://github.com/arcvra">Github</a></li>
-                    <li><a href="https://www.notion.so/PANELLS-Plantillas-1e7e9aaef00a8037837cdbbeac582cdb?pvs=4">Notion</a></li>
+                    <li>
+                        <a className="items-baseline" target="_blank" href="https://github.com/arcvra">
+                            <FaGithub />
+                        </a>
+                    </li>
+                    <li>
+                        <a className="items-baseline" target="_blank" href="https://www.notion.so/PANELLS-Plantillas-1e7e9aaef00a8037837cdbbeac582cdb?pvs=4">
+                            <RiNotionFill />
+                        </a>
+                    </li>
                 </ul>
             </div>
         </header>
