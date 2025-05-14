0e81858f10b00e6d2c56a7f89502a3ae1c1355e4
Renato <renato.vergara.dev@gmail.com>
2025-05-14 22:08:36 +0200

    feat(layout): add footer component and update layout structure

---
+++ client/app/layout/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="footer-layout bg-zinc-900 grid grid-cols-1 grid-rows-2 items-center justify-items-center py-3 text-xs">
+            <p>&copy; Renato R. Vergara 2025 </p>
+            <p>Made by <span><a href="https://github.com/arcvra" target="blank">arcvra</a></span> with love</p>
+        </footer>
+    )
+}
