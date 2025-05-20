03f8f50e48a5f7281423242daa687957e1513faa
Renato <renato.vergara.dev@gmail.com>
2025-05-20 14:20:11 +0200

    feat(CustomTabs): set tabs as "predeterminado", "firma", "completo", "disculpas", "corrección" and "formato"
    - Changed default tab value to 'predeterminado' and reorder tab triggers

--- client/components/CustomTabs.jsx
+++ client/components/CustomTabs.jsx
@@ -4,16 +4,38 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export function CustomTabs({ file }) { // TODO: add categories and style
     return (
-        <Tabs defaultValue="completo">
+        <Tabs defaultValue="predeterminado">
             <TabsList>
-                <TabsTrigger value="completo" >Completo</TabsTrigger>
+                <TabsTrigger value="predeterminado" >Predeterminado</TabsTrigger>
                 <TabsTrigger value="firma">Firma</TabsTrigger>
+                <TabsTrigger value="completo" >Completo</TabsTrigger>
+                <TabsTrigger value="disculpas">Disculpas</TabsTrigger>
+                <TabsTrigger value="corrección">Corrección</TabsTrigger>
+                <TabsTrigger value="formato">Formato</TabsTrigger>
             </TabsList>
+
+            <TabsContent value="predeterminado">
+                <MapItems items={filterByCategory({ file: file, tag: "predeterminado" })} />
+            </TabsContent>
+
+            <TabsContent value="firma">
+                <MapItems items={filterByCategory({ file: file, tag: "firma" })} />
+            </TabsContent>
+
             <TabsContent value="completo">
                 <MapItems items={filterByCategory({ file: file, tag: "completo" })} />
             </TabsContent>
-            <TabsContent value="firma">
-                <MapItems items={filterByCategory({ file: file, tag: "firma" })} />
+
+            <TabsContent value="disculpas">
+                <MapItems items={filterByCategory({ file: file, tag: "disculpas" })} />
+            </TabsContent>
+
+            <TabsContent value="corrección">
+                <MapItems items={filterByCategory({ file: file, tag: "corrección" })} />
+            </TabsContent>
+
+            <TabsContent value="formato">
+                <MapItems items={filterByCategory({ file: file, tag: "formato" })} />
             </TabsContent>
         </Tabs>
 
