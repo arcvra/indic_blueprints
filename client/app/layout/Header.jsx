c2aec6cd7fc5cc92d4525fa1f82c6f567b3bb917
Renato <renato.vergara.dev@gmail.com>
2025-05-21 16:04:26 +0200

    feat(Header): update layout and add navigation links
    - Add navigation to "/" and "/comentaris" pages

--- client/app/layout/Header.jsx
+++ client/app/layout/Header.jsx
@@ -1,13 +1,26 @@
+import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import { RiNotionFill } from "react-icons/ri";
 import { CustomInput } from "../../components/CustomInput";
 
 export default function Header() {
     return (
-        <header className="header-layout bg-black-900 grid grid-cols-[1fr_minmax(60px,5rem)_auto] gap-10 px-20 py-5 items-center">
+        <header className="header-layout bg-black-900 grid grid-cols-[1fr_minmax(120px,20rem)_auto] gap-10 px-20 py-5 items-center">
             <h1 className="font-bold text-2xl">Plantillas de texto</h1>
             <div className="flex justify-center w-full">
                 <ul className="flex gap-4 justify-around w-full">
+                    <li>
+                        <Link
+                            href="/"
+                            prefetch={true}
+                        >Correo</Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/comentaris"
+                            prefetch={true}
+                        >Comentarios tipo</Link>
+                    </li>
                     <li>
                         <a className="items-baseline" target="_blank" href="https://github.com/arcvra">
                             <FaGithub className="h-5 w-full" />
