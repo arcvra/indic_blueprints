ebfd2b2d034527c89e34271707ed8ec6e32ad378
Renato <renato.vergara.dev@gmail.com>
2025-05-21 16:41:31 +0200

    feat(Header): add vertical separators to navigation links for better user experience

--- client/app/layout/Header.jsx
+++ client/app/layout/Header.jsx
@@ -2,25 +2,31 @@ import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import { RiNotionFill } from "react-icons/ri";
 import { CustomInput } from "../../components/CustomInput";
+import { Separator } from "@/components/ui/separator"
+
 
 export default function Header() {
     return (
-        <header className="header-layout bg-black-900 grid grid-cols-[1fr_minmax(120px,20rem)_auto] gap-10 px-20 py-5 items-center">
+        <header className="header-layout bg-black-900 grid grid-cols-[1fr_minmax(180px,22rem)_auto] gap-10 px-20 py-5 items-center">
             <h1 className="font-bold text-2xl">Plantillas de texto</h1>
             <div className="flex justify-center w-full">
-                <ul className="flex gap-4 justify-around w-full">
+
+                <ul className="flex gap-3 justify-around w-full">
+                    <Separator orientation="vertical" />
                     <li>
                         <Link
                             href="/"
                             prefetch={true}
                         >Correo</Link>
                     </li>
+                    <Separator orientation="vertical" />
                     <li>
                         <Link
                             href="/comentaris"
                             prefetch={true}
                         >Comentarios tipo</Link>
                     </li>
+                    <Separator orientation="vertical" />
                     <li>
                         <a className="items-baseline" target="_blank" href="https://github.com/arcvra">
                             <FaGithub className="h-5 w-full" />
@@ -31,6 +37,7 @@ export default function Header() {
                             <RiNotionFill className="h-5 w-full" />
                         </a>
                     </li>
+                    <Separator orientation="vertical" />
                 </ul>
             </div>
             <div>
