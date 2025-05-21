55415c0d55aa80b9e9cab81ae85c7c06b9375788
Renato <renato.vergara.dev@gmail.com>
2025-05-21 15:47:02 +0200

    feat(CustomInput): disable unused Header input and button

--- client/components/CustomInput.jsx
+++ client/components/CustomInput.jsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import {TfiSearch} from "react-icons/tfi"
+import { TfiSearch } from "react-icons/tfi"
 
 export function CustomInput() {
     return (
         <div className="flex w-full max-w-sm items-center space-x-2">
-            <Input type="text" placeholder="Palabra clave"/>
-            <Button type="submit">
+            <Input type="text" placeholder="Palabra clave" disabled />
+            <Button type="submit" disabled >
                 <TfiSearch />
             </Button>
         </div>
